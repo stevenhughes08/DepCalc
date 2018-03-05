17a3529bae618be258a5b655f946d56bf289bad3
srhug <stevenhughes08@gmail.com>
2018-03-04 22:48:02 -0500

    Added InvItemViewModel this should allow the items to display on the page when the page returns.

---
+++ DepCalc/Models/InvItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DepCalc.Models
+{
+    public class InvItemViewModel
+    {
+        public int? InvItemId { get; set; }
+        public string InvItemName { get; set; }
+        public string GenLedger { get; set; }
+        public int QtyServUnit { get; set; }
+        public int QtyCountUnit { get; set; }
+        public string PurchUnit { get; set; }
+        public string CountUnit { get; set; }
+        public string SellUnit { get; set; }
+        public string CountFrequency { get; set; }
+        public int StandCost { get; set; }
+
+}
