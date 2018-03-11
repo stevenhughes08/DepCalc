b21ec0a4dd6165dfb3c36f8f4fa64bb52c0dd254
srhug <stevenhughes08@gmail.com>
2018-03-10 22:18:43 -0500

    Updated Item view Model to better reflect data.

--- DepCalc/Models/ItemViewModel.cs
+++ DepCalc/Models/ItemViewModel.cs
@@ -10,17 +10,21 @@ namespace DepCalc.Models
     public class ItemViewModel
     {
         public int? InvItemId { get; set; }
+      //  [DisplayName("Item Name")]
         public string InvItemName { get; set; }
+       // [DisplayName("Gen #")]
         public string GenLedger { get; set; }
-        public int QtyServUnit { get; set; }
-        public int QtyCountUnit { get; set; }
+       // [DisplayName("Amount Served")]
+        public double QtyServUnit { get; set; }
+        public string QtyCountUnit { get; set; }
+        public double QtyCount { get; set; }
+       // [DisplayName("Item is purchased by")]
         public string PurchUnit { get; set; }
         public string CountUnit { get; set; }
         public string SellUnit { get; set; }
         public string CountFrequency { get; set; }
-        public int StandCost { get; set; }
+        public double StandCost { get; set; }
         public object ItemProdInfo => "ID: " + InvItemId + " | " + "Item Name: " + InvItemName + " | " + QtyServUnit + " " + " per " + CountUnit + "| " + "This item is sold by " + SellUnit + " " + QtyServUnit + " per " + QtyCountUnit + " " + PurchUnit + "| " + "By Default this item is counted " + CountFrequency;
-
     }
 }
 
