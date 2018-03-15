96c331d300534f682088430cc62479d81fb24538
srhug <stevenhughes08@gmail.com>
2018-03-14 20:54:41 -0400

    Added Enitity and Migration.

--- DepCalc/Models/Item.cs
+++ DepCalc/Models/Item.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,6 +11,7 @@ namespace DepCalc.Models
 {
     public class Item
     {
+        [Key]
         public int InvItemId { get; set; }
         public string InvItemName { get; set; }
         public string GenLedger { get; set; }
