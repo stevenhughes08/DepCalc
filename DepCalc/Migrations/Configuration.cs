dadf49956997ecd605acc603b409dd53e40f14a0
stevenhughes08 <stevenhughes08@gmail.com>
2018-03-21 11:32:51 -0400

    Enabled Migrations

---
+++ DepCalc/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace DepCalc.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DepCalc.DepCalcContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(DepCalc.DepCalcContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
